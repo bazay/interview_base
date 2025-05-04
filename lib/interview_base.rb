843f3f11bdc175a5986483a0800b8a0e5fc111ce
Baron Bloomer <baronbloomer@gmail.com>
2025-05-04 09:58:56 +0800

    Add InterviewBase::Program#with_timings method

--- lib/interview_base.rb
+++ lib/interview_base.rb
@@ -11,14 +11,27 @@ module InterviewBase
     end
 
     def call
-      debug_message("Debug message: Program started")
-      # write your code here
+      with_timings do
+        debug_message("Debug message: Program started")
+        # write your code here
+      end
     end
 
     private
 
     def debug_message(message)
       puts message if @debug
+    end
+
+    def with_timings(&block)
+      caller_method_name = self.class.name + "##{caller_locations(1,1)[0].label}"
+      start_time = Time.now
+      result = block.call
+      end_time = Time.now
+      elapsed_time = end_time - start_time
+      debug_message("#{caller_method_name} - Elapsed time: #{elapsed_time} seconds")
+
+      result
     end
   end
 end
--- spec/interview_base_spec.rb
+++ spec/interview_base_spec.rb
@@ -4,15 +4,23 @@ require_relative "../lib/interview_base"
 
 RSpec.describe InterviewBase::Program do
   let(:described_instance) { described_class.new(debug: debug) }
-  let(:debug) { true }
+  let(:debug) { false }
 
   describe "#call" do
     subject(:call) { described_instance.call }
 
     it "does something useful"
 
-    it "shows debug message" do
-      expect { subject }.to output(/Debug message: Program started/).to_stdout
+    context "when debug is true" do
+      let(:debug) { true }
+
+      it "shows debug message" do
+        expect { subject }.to output(/Debug message: Program started/).to_stdout
+      end
+
+      it "shows elapsed time with caller" do
+        expect { subject }.to output(/InterviewBase::Program#call - Elapsed time:/).to_stdout
+      end
     end
   end
 end
